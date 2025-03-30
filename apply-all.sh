033f955171f1394f74e3325d77e346be8a119dfb
“Bogdan <bogdan.daniel.ioan@outlook.com>
2025-03-30 18:30:53 +0300

    removed

--- apply-all.sh
+++ apply-all.sh
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env bash
-set -e
-echo "Applying Kubernetes manifests..."
-kubectl apply -f configmap.yaml
-kubectl apply -f secret.yaml
-kubectl apply -f pv-pvc.yaml
-kubectl apply -f mysql-statefulset.yaml
-kubectl apply -f deployment-api.yaml
-kubectl apply -f deployment-kube-land.yaml
-kubectl apply -f deployment-client.yaml
-kubectl apply -f ingress.yaml
-echo "All manifests have been successfully applied."
-=======
 #!/bin/bash
 echo "🚀 Deploying your application..."
 kubectl apply -f storage.yaml
@@ -28,4 +14,3 @@ kubectl apply -f setup.yaml
 kubectl apply -f elasticsearch.yaml
 kubectl apply -f kibana.yaml
 kubectl apply -f logstash.yaml
->>>>>>> feat/implement-elk-refactor
