55882e4af1caca4d73407daf6911c3725681afc4
Bogdan Daniel Ioan <bogdan.daniel.ioan@outlook.com>
2025-06-04 15:40:54 +0300

    fix: resolve ELK stack setup job getting stuck

--- k8s-files-raw/apply-all.sh
+++ k8s-files-raw/apply-all.sh
@@ -10,8 +10,8 @@ kubectl apply -f command-service.yaml
 kubectl apply -f app-client.yaml
 kubectl apply -f ingress.yaml
 kubectl apply -f zipkin.yaml
-kubectl apply -f setup.yaml
 kubectl apply -f elasticsearch.yaml
+kubectl apply -f setup.yaml
 kubectl apply -f kibana.yaml
 kubectl apply -f logstash.yaml
 kubectl apply -f prometheus.yaml
